3749e14cfdff54feccee5b87bdcbcb6040a91e08
Samir Mehta <sammehta88@gmail.com>
2015-11-17 01:38:51 -0500

    Add create action to doses controller

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,5 +1,13 @@
 class DosesController < ApplicationController
   def new
     @animal = Animal.find(params[:animal_id])
+    @dose = @animal.doses.new
+  end
+
+  def create
+    @drug = Drug.find(params[:dose]["drug_id"])
+    @animal = Animal.find(params[:animal_id])
+    @dosage = ( @animal.weight / 10.0 ) * @drug.dosing
+    @animal.doses.create(drug: @drug, dosage: @dosage)
   end
 end
