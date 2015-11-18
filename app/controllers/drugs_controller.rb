f9efaef09a97d4f871fd0bc6eb5da6b7fc8b280c
Samir Mehta <sammehta88@gmail.com>
2015-11-17 23:27:41 -0500

    Add drugs index and show views and actions

--- app/controllers/drugs_controller.rb
+++ app/controllers/drugs_controller.rb
@@ -1,10 +1,15 @@
 class DrugsController < ApplicationController
   def index
+    @drugs = Drug.all
   end
 
   def new
   end
 
   def create
+  end
+
+  def show
+    @drug = Drug.find(params[:id])
   end
 end
