056fa1b04482c510a4aba574db205a39c74a9d49
Alice Frumin <alifrumin@yahoo.com>
2015-11-19 16:58:01 -0500

    code review

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119051352) do
+ActiveRecord::Schema.define(version: 20151118004355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 20151119051352) do
     t.string "route"
     t.string "restrictions"
     t.text   "notes"
-    t.string "image_url"
   end
 
   create_table "roles", force: :cascade do |t|
